fc3af1250b1d28f2a73e41d636a4e430adc1ae92
Hoybaby <mbartek436@gmail.com>
2020-12-17 22:20:49 -0500

    made a route folder to hold a js to make my HTML routes connect properly. I tried to launch the website before without a server and saw endpoints so I knew i had to make a route folder for endpoints

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+
+//PORT needs to be made for a server
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/htmlRoutes.js")(app);
+
+// Syncing our sequelize models and then startin
