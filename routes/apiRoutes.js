d133f0167307cd531926cfa40d1f5955796927fa
Hoybaby <mbartek436@gmail.com>
2020-12-18 01:25:59 -0500

    completed my first api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,23 @@
 
+const router = require('express').Router();
+const db = require("../models");
+
+
+router.get("/workouts", (req,res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+
+})
 
 module.exports = function(app) {
 
     app.get("/api/authors", function(req,res) {
-        
+
     })
 
 
